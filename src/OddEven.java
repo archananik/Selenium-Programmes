bb7814ee5e41b04c85528715b2057c03dd95e0c8
Python Coder <pythoncoder@demo.com>
2024-04-18 10:27:15 +0530

    first commit

---
+++ src/OddEven.java
@@ -0,0 +1,21 @@
+
+public class OddEven
+{
+
+
+	public static void main(String[] args) 
+	{
+	     int a=12;
+	     
+	     if(a%2==0)
+	     {
+	    	 System.out.println("number is even");
+	     }
+	     else
+	     {
+	    	 System.out.println("number is odd");
+	     }
+
+	}
+
+}
